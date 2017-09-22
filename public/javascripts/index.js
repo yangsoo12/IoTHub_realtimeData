1e9ef1402a01f67693030a9e5b178249fe5cb54f
yangsoo12 <30304805+yangsoo12@users.noreply.github.com>
2017-09-22 17:47:17 +0900

    Update index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -230,30 +230,55 @@ $(document).ready(function () {
 	  {
 		  rpm2Data.shift();
 	  }
+
+// yangji, update data Start-------------------------------------------------------
+
+if(obj.params.pm2>100){
+  document.getElementById("pm25dis").innerHTML = "아주나쁨";
+}else if(obj.params.pm2>50){
+  document.getElementById("pm25dis").innerHTML = "나쁨";
+}else if(obj.params.pm2>15){
+  document.getElementById("pm25dis").innerHTML = "보통";
+}else if(obj.params.pm2>0){
+  document.getElementById("pm25dis").innerHTML = "좋음";
+}
+if(obj.params.pm10>150){
+  document.getElementById("pm10dis").innerHTML = "아주나쁨";
+}else if(obj.params.pm2>80){
+  document.getElementById("pm10dis").innerHTML = "나쁨";
+}else if(obj.params.pm2>30){
+  document.getElementById("pm10dis").innerHTML = "보통";
+}else if(obj.params.pm2>0){
+  document.getElementById("pm10dis").innerHTML = "좋음";
+}
+
+// yangji, update data End-------------------------------------------------------
+
+
 //graph tag start------------------------------------------------------------------
-if(obj.params.pm2>80){
-	document.getElementById("pm25dis").innerHTML = "아주나쁨";
-}else if(obj.params.pm2>60){
-	document.getElementById("pm25dis").innerHTML = "나쁨";
-}else if(obj.params.pm2>40){
-	document.getElementById("pm25dis").innerHTML = "보통";
-}else if(obj.params.pm2>20){
-	document.getElementById("pm25dis").innerHTML = "좋음";
-}else if(obj.params.pm2>0){
-	document.getElementById("pm25dis").innerHTML = "아주좋음";
-}
-	    if(obj.params.pm10>80){
-	document.getElementById("pm10dis").innerHTML = "아주나쁨";
-}else if(obj.params.pm2>60){
-	document.getElementById("pm10dis").innerHTML = "나쁨";
-}else if(obj.params.pm2>40){
-	document.getElementById("pm10dis").innerHTML = "보통";
-}else if(obj.params.pm2>20){
-	document.getElementById("pm10dis").innerHTML = "좋음";
-}else if(obj.params.pm2>0){
-	document.getElementById("pm10dis").innerHTML = "아주좋음";
-}
-	    if(obj.params.Temperature>30){
+// if(obj.params.pm2>80){
+// 	document.getElementById("pm25dis").innerHTML = "아주나쁨";
+// }else if(obj.params.pm2>60){
+// 	document.getElementById("pm25dis").innerHTML = "나쁨";
+// }else if(obj.params.pm2>40){
+// 	document.getElementById("pm25dis").innerHTML = "보통";
+// }else if(obj.params.pm2>20){
+// 	document.getElementById("pm25dis").innerHTML = "좋음";
+// }else if(obj.params.pm2>0){
+// 	document.getElementById("pm25dis").innerHTML = "아주좋음";
+// }
+// 	    if(obj.params.pm10>80){
+// 	document.getElementById("pm10dis").innerHTML = "아주나쁨";
+// }else if(obj.params.pm2>60){
+// 	document.getElementById("pm10dis").innerHTML = "나쁨";
+// }else if(obj.params.pm2>40){
+// 	document.getElementById("pm10dis").innerHTML = "보통";
+// }else if(obj.params.pm2>20){
+// 	document.getElementById("pm10dis").innerHTML = "좋음";
+// }else if(obj.params.pm2>0){
+// 	document.getElementById("pm10dis").innerHTML = "아주좋음";
+// }
+if(obj.params.Temperature>30){
 	document.getElementById("tempdis").innerHTML = "높음";
 }else if(obj.params.Temperature>20){
 	document.getElementById("tempdis").innerHTML = "쾌적";
@@ -326,11 +351,11 @@ if(obj.params.pm2>80){
       //android 20170912 23:29
       function insertDatas(p2,p1,t,h){
          var p2State;
-         if(p2<31){
+         if(p2<16){
            p2State = "좋음";
-         }else if(p2<81){
+         }else if(p2<51){
            p2State = "보통";
-         }else if(p2<151){
+         }else if(p2<101){
            p2State = "나쁨";
          }else{
            p2State = "매우나쁨";
